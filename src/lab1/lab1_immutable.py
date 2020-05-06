b5736c26ea4d432f4e020200a8c27160d6847e3f
王嘉奕 <wangjiayi@wangjiayideMacBook-Pro.local>
2020-05-06 22:24:16 +0800

    update

---
+++ src/lab1/lab1_immutable.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*- 
+
+class Node(object):
+    def __init__(self, value=None, lchild=None, rchild=None):
+        self.value  = value
+        self.lchild = lchild
+        self.rchild = rchild
+    
+    def __eq__(self, other):
+        if id(self) == id(other):
+            return True
+        else:
+            lst_self  = create_none_list(self)
+            lst_other = create_none_list(other)
+            to_list(self, 0, lst_self)
+            to_list(other, 0, lst_other)
+            return lst_self == lst_other
+            
+    
+def create_none_list(node):
+    if get_size(node) == 1:
+        return [None, None, None]
+    else:
+        return [None for _ in range((1<<get_depth(node))-1)]
+
+def to_list(node, cntr, lst):
+    if cntr <= len(lst) - 1:
+        if node is not None:
+            lst[cntr] = node.value
+        else:
+            lst[cntr] = None
+            return
+        to_list(node.lchild, (cntr<<1)+1, lst)
+        to_list(node.rchild, (cntr<<1)+2, lst)
+    else:
+        return
+
+def from_list(lst, index):
+    if index < len(lst):
+        return Node(value=lst[index], lchild=from_list(lst, (index<<1)+1), rchild=from_list(lst, (index<<1)+2))
+
+
+def is_leaf(node):
+    if node.lchild == None and node.rchild == None:
+        return True
+    else:
+        return False
+
+def get_depth(node):
+    if node is None:
+        return 0
+    else:
+        return max(get_depth(node.lchild)+1, get_depth(node.rchild)+1)
+
+
+def get_size(node):
+    # Get size of the binary tree.
+    if node is None:
+        return 0
+    elif is_leaf(node):
+        return 1
+    else:
+        return get_size(node.lchild) + get_size(node.rchild) + 1
+
+def add_node(value, left=None, right=None):
+    # Add node into the binary tree.
+    return Node(value=value, lchild=left, rchild=right)
+
+def remove_node(node, element):
+    """
+    Remove node from the tree.
+    If the node is a leaf, it will be removed directly. Otherwise, node with its children will be all removed.
+    Parameters:
+        node: root of tree.
+        element: the element that should be removed.
+    """
+    lst = [None for _ in range((1<<get_depth(node))-1)]
+    to_list(node, 0, lst)
+    index = lst.index(element)
+    parent_index = index >> 1
+    parent_element = lst[parent_index]
+    find_parent(node, parent_element)
+
+def find_parent(node, element_value):
+    if node is not None:
+        if node.value == element_value:
+            node.lchild = None
+            node.rchild = None
+        find_parent(node.lchild, element_value)
+        find_parent(node.rchild, element_value)
+def find_node(node, element_value, fetch_list):
+    if node is not None:
+        if node.value == element_value:
+            fetch_list.append(node.value)
+        find_node(node.lchild, element_value, fetch_list)
+        find_node(node.rchild, element_value, fetch_list)
+
+
+def mempty():
+    return None
+
+def filter(node, func):
+    lst = [None for x in range((1<<get_depth(node))-1)]
+    to_list(node, 0, lst)
+    new_lst = func(lst)
+    return new_lst
+
+def map_func(lst):
+    for i in range(len(lst)):
+        if lst[i] is not None:
+            lst[i] = lst[i] << 1 + 2
+def reduce_fuc(x, y):
+    return x + y
+
+def filter_func(lst):
+    new_list = []
+    for i in range(len(lst)):
+        if lst[i] is not None and lst[i] <= 'C':
+            new_list.append(lst[i])
+    return new_list
+
+def map(node, func):
+    lst = [None for x in range((1<<get_depth(node))-1)]
+    to_list(node, 0, lst)
+    func(lst)
+    return from_list(lst, 0)
+
+def my_reduce(node, func):
+    from functools import reduce
+    lst = [None for x in range((1<<get_depth(node))-1)]
+    to_list(node, 0, lst)
+    new_list = [x for x in lst if x is not None]
+    return reduce(func, new_list)
+
+def iterator(node):
+    lst = create_none_list(node)
+    to_list(node, 0, lst)
+    return iter(lst)
---
+++ src/lab1/test_immutable.py
@@ -0,0 +1,73 @@
+from lab1_immutable import *
+import unittest
+
+class TestImmutableMutableList(unittest.TestCase):
+    def test_size(self):
+        self.assertEqual(get_size(None), 0)
+        self.assertEqual(get_size(Node(0)), 1)
+        self.assertEqual(get_size(Node(value=1, lchild=add_node(value=2))), 2)
+    
+    def test_get_depth(self):
+        self.assertEqual(get_depth(Node(value=1)), 1)
+        self.assertEqual(get_depth(Node(value=1, lchild=Node(value=2), rchild=Node(value=3))), 2)
+        self.assertEqual(get_depth(add_node(value=1, left=add_node(value=2, left=add_node(value=3)))), 3)
+
+    def test_create_none_list(self):
+        self.assertEqual(create_none_list(Node(value=1)), [None, None, None])
+    
+    def test_to_list(self):
+        node_1 = Node(value=1)
+        lst_node_1 = create_none_list(node_1)
+        to_list(node_1, 0, lst_node_1)
+        self.assertEqual(lst_node_1, [1, None, None])
+
+        node_2 = add_node(value=1, left=add_node(value=2), right=add_node(value=3, left=Node(value=4)))
+        lst_node_2 = create_none_list(node_2)
+        to_list(node_2, 0, lst_node_2)
+        self.assertEqual(lst_node_2, [1, 2, 3, None, None, 4, None])
+
+    def test_from_list(self):
+        list_1 = [1, 2, 3, None, None, 4, None]
+        node_1 = add_node(value=1, left=add_node(value=2), right=add_node(value=3, left=Node(value=4))) 
+        self.assertEqual(from_list(list_1, 0), node_1)
+
+    def test_add_node(self):
+        self.assertEqual(Node(1), add_node(1))
+        self.assertEqual(Node(1, lchild=Node(2)), add_node(1, left=add_node(2)))
+    def test_find_node(self):
+        node_1 = add_node(value=1, left=add_node(value=2), right=add_node(value=3, left=Node(value=4)))
+        fetch_list = []
+        find_node(node_1, 2, fetch_list)
+        self.assertEqual(fetch_list, [2])
+    
+    def test_remove_node(self):
+        node = Node(1, lchild=Node(2))
+        remove_node(node, 2)
+        self.assertEqual(node, Node(1))
+
+    def test_iterator(self):
+        node = add_node(value=1, left=add_node(value=2), right=add_node(value=3, left=Node(value=4)))
+        iter_lst = []
+        it = iterator(node)
+        while True:
+            try:
+                x = next(it)
+                iter_lst.append(x)
+            except StopIteration:
+                break
+        self.assertEqual(iter_lst, [1, 2, 3, None, None, 4, None])
+
+    from hypothesis import given
+    import hypothesis.strategies as st
+    @given(node=st.lists(st.integers()))
+    def test_equality(self, node):
+        tree = from_list(node, 0)
+        new_lst = create_none_list(tree)
+        to_list(tree, 0, new_lst)
+        lst = []
+        for i in range(len(new_lst)):
+            if new_lst[i] != None:
+                lst.append(new_lst[i])
+        self.assertEqual(lst, node)
+if __name__ == '__main__':
+    unittest.main()
